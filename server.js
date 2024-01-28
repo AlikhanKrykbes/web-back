4bfb3f52c4c95e2ea154904d90ef5a8bae97a5b5
AlikhanKrykbes <123235420+AlikhanKrykbes@users.noreply.github.com>
2024-01-29 02:40:31 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +17,121 @@ const bookRoute = require('./public/routes/book');
 
 app.use('/home', homeRoute);
 app.use('/book', bookRoute);
+app.get('/weather', async (req, res) => {
+    try {
+        const apiKey = '7f0d1f04613c4c6b91b65926241901'; 
+        const city = 'London';
+
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`);
+
+        const temperature = response.data.current.temp_c; 
+
+        const weatherData = {
+            temperature: temperature,
+        };
+
+        res.json(weatherData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/weather2', async (req, res) => {
+    try {
+        const apiKey = '7f0d1f04613c4c6b91b65926241901'; 
+        const city2 = 'Paris';
+
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city2}&aqi=no`);
+
+        const temperature = response.data.current.temp_c; 
+
+        const weatherData = {
+            temperature: temperature,
+        };
+
+        res.json(weatherData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/weather3', async (req, res) => {
+    try {
+        const apiKey = '7f0d1f04613c4c6b91b65926241901'; 
+        const city3 = 'Islamabad';
+
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city3}&aqi=no`);
+
+        const temperature = response.data.current.temp_c; 
+
+        const weatherData = {
+            temperature: temperature,
+        };
+
+        res.json(weatherData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/weather4', async (req, res) => {
+    try {
+        const apiKey = '7f0d1f04613c4c6b91b65926241901'; 
+        const city4 = 'Rome';
+
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city4}&aqi=no`);
+
+        const temperature = response.data.current.temp_c; 
+
+        const weatherData = {
+            temperature: temperature,
+        };
+
+        res.json(weatherData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/weather5', async (req, res) => {
+    try {
+        const apiKey = '7f0d1f04613c4c6b91b65926241901'; 
+        const city5 = 'Agra';
+
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city5}&aqi=no`);
+
+        const temperature = response.data.current.temp_c; 
+
+        const weatherData = {
+            temperature: temperature,
+        };
+
+        res.json(weatherData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/weather6', async (req, res) => {
+    try {
+        const apiKey = '7f0d1f04613c4c6b91b65926241901'; 
+        const city6 = 'Washington'; 
+
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city6}&aqi=no`);
+
+        const temperature = response.data.current.temp_c; 
+
+        const weatherData = {
+            temperature: temperature,
+        };
+
+        res.json(weatherData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}/home`);
 });
